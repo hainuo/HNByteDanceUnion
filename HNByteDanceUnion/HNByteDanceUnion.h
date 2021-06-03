95cceae8d9af6a2aa9c1d92f61c84745275bf8b2
hs <hls@lhdl.com>
2021-06-03 22:49:11 +0800

    init

---
+++ HNByteDanceUnion/HNByteDanceUnion.h
@@ -0,0 +1,12 @@
+//
+//  HNByteDanceUnion.h
+//  HNByteDanceUnion
+//
+//  Created by hainuo on 2021/4/2.
+//
+
+#import <Foundation/Foundation.h>
+#import "UZEngine/UZModule.h"
+@interface HNByteDanceUnion : UZModule
+
+@end
